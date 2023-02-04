14d3f3e2e722d14d64a2fd8268a19184af18ab80
emadhussein3@gmail.com <emadhussein3@gmail.com>
2023-02-04 19:32:37 +0300

    Add project files.

---
+++ WorkoutApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkoutApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define a list of workouts
+            List<Workout> workouts = new List<Workout>
+            {
+                new Workout { Name = "Chest & Triceps", Exercises = new List<string> { "Barbell Bench Press", "Dumbbell Flys", "Tricep Dips" } },
+                new Workout { Name = "Back & Biceps", Exercises = new List<string> { "Barbell Deadlifts", "Bent Over Rows", "Barbell Bicep Curls" } },
+                new Workout { Name = "Legs & Shoulders", Exercises = new List<string> { "Squats", "Lunges", "Shoulder Press" } }
+            };
+
+            // Display the workouts to the user
+            Console.WriteLine("Workouts:");
+            for (int i = 0; i < workouts.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {workouts[i].Name}");
+            }
+
+            // Ask the user to select a workout
+            Console.WriteLine("\nSelect a workout by entering its number:");
+            int workoutIndex = int.Parse(Console.ReadLine()) - 1;
+
+            // Display the exercises for the selected workout
+            Console.WriteLine($"\n{workouts[workoutIndex].Name} Exercises:");
+            foreach (string exercise in workouts[workoutIndex].Exercises)
+            {
+                Console.WriteLine(exercise);
+            }
+        }
+    }
+
+    class Workout
+    {
+        public string Name { get; set; }
+        public List<string> Exercises { get; set; }
+    }
+}
